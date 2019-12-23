d7ae9dc58d88a927c7218690449ffa766be34989
Claudia Cadena <yayitax@users.noreply.github.com>
2019-12-23 13:39:53 -0700

    Finished module 4

--- routes/bookRouter.js
+++ routes/bookRouter.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-param-reassign */
 const express = require('express');
 
 function routes(Book) {
@@ -20,13 +21,61 @@ function routes(Book) {
         return res.json(books);
       });
     });
+  bookRouter.use('/books/:bookId', (req, res, next) => {
+    Book.findById(req.params.bookId, (err, book) => {
+      if (err) {
+        return res.send(err);
+      }
+      if (book) {
+        req.book = book;
+        return next();
+      }
+      return res.sendStatus(404);
+    });
+  });
   bookRouter.route('/books/:bookId')
-    .get((req, res) => {
-      Book.findById(req.params.bookId, (err, book) => {
+    .get((req, res) => res.json(req.book))
+    .put((req, res) => {
+      const { book } = req;
+      book.title = req.body.title;
+      book.author = req.body.author;
+      book.genre = req.body.genre;
+      book.read = req.body.read;
+      req.book.save((err) => {
         if (err) {
           return res.send(err);
         }
         return res.json(book);
+      });
+    })
+    .patch((req, res) => {
+      const { book } = req;
+
+      // eslint-disable-next-line no-underscore-dangle
+      if (req.body._id) {
+        // eslint-disable-next-line no-underscore-dangle
+        delete req.body._id;
+      }
+
+      Object.entries(req.body).forEach((item) => {
+        const key = item[0];
+        const value = item[1];
+        book[key] = value;
+      });
+
+      req.book.save((err) => {
+        if (err) {
+          return res.send(err);
+        }
+        return res.json(book);
+      });
+    })
+    .delete((req, res) => {
+      req.book.remove((err) => {
+        if (err) {
+          return res.send.err();
+        }
+        return res.sendStatus(204);
       });
     });
 
